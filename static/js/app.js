2b28b810926b53198fbe9c907e5049810fb0f349
Andrew Theiss <aftheiss@swbell.net>
2021-06-18 18:35:20 -0500

    final

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,38 @@
 // from data.js
 var tableData = data;
+var tbody = d3.select("tbody");
 
-// YOUR CODE HERE!
+function buildTable(arrayData) {
+arrayData.forEach((ufoReport) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoReport).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+  });
+}
+
+var button = d3.select("#filter-btn");
+
+button.on("click", filterTable);
+
+function filterTable() {
+
+  d3.event.preventDefault();
+  
+  var inputElement = d3.select("#datetime");
+
+  var inputValue = inputElement.property("value");
+  console.log(inputValue);
+
+  var filteredData = tableData.filter(ufo => ufo.datetime === inputValue);
+    
+tbody.html("")
+
+  buildTable(filteredData);
+}
+
+
+
+
+buildTable(tableData)
